7c76d360f1d8629e3bf89dea1a608468c34e7611
JIrayut Khantavee <ker.koleng@hotmail.co.th>
2018-09-21 15:27:16 +0700

    restruct and apply middleware

---
+++ controllers/token/index.js
@@ -0,0 +1,14 @@
+const app = require('express')
+const router = app.Router()
+
+const { index, store } = require('./token')
+
+const { myBasicAuthorizer } = require('./middleware')
+const basicAuth = require('express-basic-auth')
+
+router.use(basicAuth({ authorizer: myBasicAuthorizer }))
+
+router.get('/', index)
+router.post('/', store)
+
+module.exports = router
