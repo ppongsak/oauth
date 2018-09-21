4ad7ddfb20b01d33ef9435ee5015e1217addc5ec
Pongsak Prabparn <pongsak@Pongsaks-MacBook-Pro.local>
2018-09-21 17:07:28 +0700

    edit basic auth

--- controllers/token/middleware.js
+++ controllers/token/middleware.js
@@ -1,10 +1,29 @@
-// require mysql lib
+const { getClientsByUsername } = require('../../models/oauthClients')
 
-const myBasicAuthorizer = (username, password) => {
-  // select database
-  console.log('myBasicAuthorizer', username, password)
-  //return true or false
-  return username.startsWith('A') && password.startsWith('secret')
+const myBasicAuthorizer = async (username, password) => {
+    const clients = await getClientsByUsername(username, password)
+
+    // var client_id
+    // var client_secret
+
+    // if (clients.length != 1) {
+    //     client_id = null
+    //     client_secret = null
+    // } else {
+    //     client_id = clients[0].client_id
+    //     client_secret = clients[0].client_secret
+    // }
+
+    // console.log(
+    //     username.startsWith(client_id) && password.startsWith(client_secret)
+    // )
+
+    // if (!(username.startsWith(client_id) && password.startsWith(client_secret)))
+    //     return res.status(400).json({
+    //         status: false
+    //     })
+
+    return username.startsWith('A') && password.startsWith('secret')
 }
 
 module.exports = { myBasicAuthorizer }
--- models/basicAuthen.js
+++
@@ -1,7 +0,0 @@
-const db = require('../configs/mysql')
-
-const validateClientCredentials = req => {
-    // let clientId = req.params.username
-    // let clientPassword = req.params.password
-    console.log(req.params)
-}
